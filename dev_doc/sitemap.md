204cc3317a8d088564e03a30f0775f02accca219
hibana2077 <hibana2077@gmaill.com>
2023-12-19 16:04:55 +0800

    Add flowbite-svelte-blocks and flowbite-svelte-icons dependencies

---
+++ dev_doc/sitemap.md
@@ -0,0 +1,43 @@
+<!--
+ * @Author: hibana2077 hibana2077@gmaill.com
+ * @Date: 2023-12-19 16:03:57
+ * @LastEditors: hibana2077 hibana2077@gmaill.com
+ * @LastEditTime: 2023-12-19 16:04:41
+ * @FilePath: /NTTU_AI_CUP/dev_doc/sitemap.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# Sitemap
+
+1. **首頁**
+   - 介紹平台和比賽概述
+   - 最新消息和更新
+   - 登入/註冊入口
+
+2. **比賽信息**
+   - 當前和即將進行的比賽列表
+   - 比賽細節（規則、時間表、獎項等）
+   - 歷史比賽檔案
+
+3. **參賽隊伍/個人區**
+   - 注冊新隊伍或個人參賽
+   - 管理隊伍信息（如果適用）
+   - 上傳和管理提交的作品
+
+4. **資源中心**
+   - 教程和指南
+   - AI 相關知識庫
+   - 常見問題解答
+
+5. **社區互動**
+   - 討論版
+   - 活動日曆
+   - 同行評論和建議區
+
+6. **後台管理**
+   - 比賽創建和管理
+   - 參賽者管理
+   - 網站內容管理
+
+7. **聯絡我們**
+   - 客服和技術支持
+   - 反饋和建議提交
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,13 +2,13 @@
  * @Author: hibana2077 hibana2077@gmaill.com
  * @Date: 2023-12-19 15:32:49
  * @LastEditors: hibana2077 hibana2077@gmaill.com
- * @LastEditTime: 2023-12-19 15:41:06
+ * @LastEditTime: 2023-12-19 15:48:12
  * @FilePath: /NTTU_AI_CUP/frontend/tailwind.config.cjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte-blocks/**/*.{html,js,svelte,ts}'],
 
   plugins: [require('flowbite/plugin')],
 
