b2c92ca7de698d192093640b3bcf882a88fe19bc
hibana2077 <hibana2077@gmail.com>
2023-12-23 23:49:23 +0800

    Update button text in +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -97,7 +97,7 @@
     <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
       <a href="/">
         <Button size="lg" color="red">
-          更多比賽 <ArrowRightSolid size="md" class="ml-2 -mr-1" />
+          報名流程 <ArrowRightSolid size="md" class="ml-2 -mr-1" />
         </Button>
       </a>
       <a href="/">
