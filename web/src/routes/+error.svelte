e340998b04e4e4948801d3e59249795e4809c304
hibana2077 <hibana2077@gmail.com>
2024-01-28 09:53:48 +0800

    Add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<main>
+    <h1 class="mb-2">404</h1>
+    <h2 class="text-gray-500 mb-2">Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <p class="mt-4">
+        <a href="./" class="text-blue-500 hover:underline">Go back home</a>.
+    </p>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 80vh;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 3rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+    }
+</style>
