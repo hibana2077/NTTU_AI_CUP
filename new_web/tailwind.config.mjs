494602c148a969008d651f4455d4f8d1c1df4990
hibana2077 <hibana2077@gmail.com>
2024-03-15 12:05:47 +0800

    Add react-icons and daisyui dependencies, update tailwind.config.mjs, and modify Card and Layout components

--- new_web/tailwind.config.mjs
+++ new_web/tailwind.config.mjs
@@ -2,7 +2,7 @@
  * @Author: hibana2077 hibana2077@gmail.com
  * @Date: 2024-03-15 09:47:24
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2024-03-15 09:56:53
+ * @LastEditTime: 2024-03-15 09:58:55
  * @FilePath: \NTTU_AI_CUP\new_web\tailwind.config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,5 +16,5 @@ export default {
 	},
 	plugins: [],
 	darkMode: "class",
-  	plugins: [nextui()]
+  	plugins: [nextui(),require("daisyui")]
 }
